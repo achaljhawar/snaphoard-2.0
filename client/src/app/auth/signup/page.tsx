8cffbc4b28f5cdf5adb81b584c07244062bfd150
achaljhawar <achaljhawar03@gmail.com>
2024-06-23 23:51:41 +0530

    feat: completed the frontend of the signup page and implemented zod input validation with it

---
+++ client/src/app/auth/signup/page.tsx
@@ -0,0 +1,230 @@
+"use client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { AuthCredentialsValidator } from "@/schema/signupmodels";
+import styled from "@emotion/styled";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast, Toaster } from "react-hot-toast";
+import { eyeClose, eyeOpen } from "@/components/icons";
+import { cookies } from "next/headers";
+
+const ErrorMessage = styled.p`
+  font-size: 0.75rem;
+  color: #dc2626;
+`;
+
+const PasswordInputWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+const PasswordInput = styled(Input)`
+  padding-right: 2.5rem; /* Make room for the icon */
+`;
+
+const IconWrapper = styled.button`
+  position: absolute;
+  right: 0.5rem;
+  background: none;
+  border: none;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  svg {
+    width: 1rem;
+    height: 1rem;
+  }
+`;
+
+export default function LoginPage() {
+  const [type, setType] = useState("password");
+  const [Icon, setIcon] = useState(() => eyeClose);
+  type TAuthCredentialValidator = z.infer<typeof AuthCredentialsValidator>;
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const handleToggle = () => {
+    if (type === "password") {
+      setIcon(() => eyeOpen);
+      setType("text");
+    } else {
+      setIcon(() => eyeClose);
+      setType("password");
+    }
+  };
+
+  const onSubmit = async (data: TAuthCredentialValidator) => {
+    try {
+      if (data) {
+        console.log("Form submitted successfully:", data);
+        toast.success(
+          "Form submitted successfully. Check your email to verify your account."
+        );
+      } else {
+        toast.error("Error submitting form. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      toast.error("An unexpected error occurred. Please try again later.");
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[90vh]">
+      <br />
+      <br />
+      <br />
+      <br />
+      <Toaster position="top-center" reverseOrder={false} />
+      <Card className="mx-auto max-w-sm gap-2 -mt-20">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="firstName">First name</Label>
+                  <Input
+                    id="firstName"
+                    placeholder="Gavin"
+                    {...register("firstName")}
+                  />
+                  {errors.firstName && (
+                    <ErrorMessage>{errors.firstName.message}</ErrorMessage>
+                  )}
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="lastName">Last name</Label>
+                  <Input
+                    id="lastName"
+                    placeholder="Belson"
+                    {...register("lastName")}
+                  />
+                  {errors.lastName && (
+                    <ErrorMessage>{errors.lastName.message}</ErrorMessage>
+                  )}
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  placeholder="gavinbelson"
+                  {...register("username")}
+                />
+                {errors.username && (
+                  <ErrorMessage>{errors.username.message}</ErrorMessage>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="gavin@hooli.com"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <ErrorMessage>{errors.email.message}</ErrorMessage>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <PasswordInputWrapper>
+                  <PasswordInput
+                    id="password"
+                    type={type}
+                    placeholder="piedpipersucks"
+                    {...register("password")}
+                  />
+                  <IconWrapper type="button" onClick={handleToggle}>
+                    <Icon />
+                  </IconWrapper>
+                </PasswordInputWrapper>
+                {errors.password && (
+                  <ErrorMessage>{errors.password.message}</ErrorMessage>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="confirmPassword">Confirm Password</Label>
+                <Input
+                  id="confirmPassword"
+                  type="password"
+                  placeholder="piedpipersucks"
+                  {...register("confirmPassword")}
+                />
+                {errors.confirmPassword && (
+                  <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="role">Role</Label>
+                <Controller
+                  name="role"
+                  control={control}
+                  render={({ field }) => (
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger className="w-[350px]">
+                        <SelectValue placeholder="Select a role" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="Viewer">Viewer</SelectItem>
+                        <SelectItem value="Poster">Poster</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  )}
+                />
+                {errors.role && (
+                  <ErrorMessage>{errors.role.message}</ErrorMessage>
+                )}
+              </div>
+              <Button type="submit" className="w-full">
+                Create an account
+              </Button>
+              <Button variant="outline" className="w-full" type="button">
+                Sign up with 3rd-part service
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link href="/auth/signin" className="underline">
+                Sign in
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- client/src/components/unauth-navbar.tsx
+++ client/src/components/unauth-navbar.tsx
@@ -23,19 +23,22 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { usePathname } from "next/navigation";
 import useDeviceDetection from "@/hooks/useDeviceDetection";
 import { Pacifico, Permanent_Marker } from "next/font/google";
+
 const marker = Permanent_Marker({
   weight: "400",
   subsets: ["latin"],
 });
+
 const pacifico = Pacifico({
   weight: "400",
   subsets: ["latin"],
 });
+
 export default function UnAuthNavbar() {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
   const { setTheme } = useTheme();
   const device = useDeviceDetection();
-  console.log(device);
+
   useEffect(() => {
     setTheme(isDarkMode ? "dark" : "light");
   }, [isDarkMode, setTheme]);
@@ -46,7 +49,6 @@ export default function UnAuthNavbar() {
 
   return (
     <header className="flex items-center justify-between p-2 sm:p-4 md:px-6 lg:px-8">
-      {" "}
       <Link href="/" className="flex items-center" prefetch={false}>
         <CameraIcon
           className={`h-6 w-6 ${isDarkMode ? "text-gray-50" : "text-gray-900"}`}
@@ -62,24 +64,23 @@ export default function UnAuthNavbar() {
           Snaphoard
         </span>
       </Link>
-      {device === "Desktop" ? (
-        <nav className="flex items-center space-x-2 sm:space-x-4 md:space-x-6">
-          {" "}
-          <Link
-            href="#"
-            className={cn(
-              `text-sm sm:text-base font-medium transition-colors ${
-                isDarkMode
-                  ? "text-gray-400 hover:text-gray-50"
-                  : "text-gray-700 hover:text-gray-900"
-              }`,
-              buttonVariants({ variant: "link" })
-            )}
-            prefetch={false}
-          >
-            Explore
-          </Link>
+      <div className="flex items-center space-x-2 sm:space-x-4">
+        {device === "Desktop" ? (
           <>
+            <Link
+              href="#"
+              className={cn(
+                `text-sm sm:text-base font-medium transition-colors ${
+                  isDarkMode
+                    ? "text-gray-400 hover:text-gray-50"
+                    : "text-gray-700 hover:text-gray-900"
+                }`,
+                buttonVariants({ variant: "link" })
+              )}
+              prefetch={false}
+            >
+              Explore
+            </Link>
             <Link
               href="#"
               className={cn(
@@ -109,86 +110,83 @@ export default function UnAuthNavbar() {
               Signup
             </Link>
           </>
-        </nav>
-      ) : (
-        <DropdownMenu>
-          <DropdownMenuTrigger
-            asChild
-            className="flex items-center space-x-4 md:space-x-6 ml-auto"
-          >
-            <Button variant="ghost" size="icon">
-              <HamburgerMenuIcon
-                className={`h-6 w-6 ${
-                  isDarkMode ? "text-gray-50" : "text-gray-900"
-                }`}
-              />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>
-              <Link
-                href="#"
-                className={cn(
-                  `text-sm sm:text-base font-medium transition-colors ${
-                    isDarkMode
-                      ? "text-gray-400 hover:text-gray-50"
-                      : "text-gray-700 hover:text-gray-900"
-                  }`,
-                  buttonVariants({ variant: "link" })
-                )}
-                prefetch={false}
-              >
-                Explore
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link
-                href="#"
-                className={cn(
-                  `text-sm sm:text-base font-medium transition-colors ${
-                    isDarkMode
-                      ? "text-gray-400 hover:text-gray-50"
-                      : "text-gray-700 hover:text-gray-900"
-                  }`,
-                  buttonVariants({ variant: "link" })
-                )}
-                prefetch={false}
-              >
-                Login
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link
-                href="#"
-                className={cn(
-                  `text-sm sm:text-base font-medium transition-colors ${
-                    isDarkMode
-                      ? "text-gray-400 hover:text-gray-50"
-                      : "text-gray-700 hover:text-gray-900"
-                  }`,
-                  buttonVariants({ variant: "link" })
-                )}
-                prefetch={false}
-              >
-                Signup
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
-      <Button
-        variant="ghost"
-        size="icon"
-        className="rounded-full ml-4"
-        onClick={handleThemeToggle}
-      >
-        {isDarkMode ? (
-          <SunIcon className="h-6 w-6 text-gray-50" />
         ) : (
-          <MoonIcon className="h-6 w-6 text-gray-900" />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <HamburgerMenuIcon
+                  className={`h-6 w-6 ${
+                    isDarkMode ? "text-gray-50" : "text-gray-900"
+                  }`}
+                />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link
+                  href="#"
+                  className={cn(
+                    `text-sm sm:text-base font-medium transition-colors ${
+                      isDarkMode
+                        ? "text-gray-400 hover:text-gray-50"
+                        : "text-gray-700 hover:text-gray-900"
+                    }`,
+                    buttonVariants({ variant: "link" })
+                  )}
+                  prefetch={false}
+                >
+                  Explore
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href="#"
+                  className={cn(
+                    `text-sm sm:text-base font-medium transition-colors ${
+                      isDarkMode
+                        ? "text-gray-400 hover:text-gray-50"
+                        : "text-gray-700 hover:text-gray-900"
+                    }`,
+                    buttonVariants({ variant: "link" })
+                  )}
+                  prefetch={false}
+                >
+                  Login
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link
+                  href="#"
+                  className={cn(
+                    `text-sm sm:text-base font-medium transition-colors ${
+                      isDarkMode
+                        ? "text-gray-400 hover:text-gray-50"
+                        : "text-gray-700 hover:text-gray-900"
+                    }`,
+                    buttonVariants({ variant: "link" })
+                  )}
+                  prefetch={false}
+                >
+                  Signup
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         )}
-        <span className="sr-only">Toggle dark mode</span>
-      </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="rounded-full"
+          onClick={handleThemeToggle}
+        >
+          {isDarkMode ? (
+            <SunIcon className="h-6 w-6 text-gray-50" />
+          ) : (
+            <MoonIcon className="h-6 w-6 text-gray-900" />
+          )}
+          <span className="sr-only">Toggle dark mode</span>
+        </Button>
+      </div>
     </header>
   );
 }
